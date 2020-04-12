fd55517d6a045b891fc7a8d464bd91f5017a271a
rishabh <rishabh@localhost>
2020-04-12 16:48:36 +0530

    Commited

---
+++ twiliovoicequickstart-1/src/main/java/com/twilio/twiliovoice/Twiliovoicequickstart1Application.java
@@ -0,0 +1,13 @@
+package com.twilio.twiliovoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class Twiliovoicequickstart1Application {
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(Twiliovoicequickstart1Application.class, args);
+//	}
+
+}
---
+++ twiliovoicequickstart-1/src/test/java/com/twilio/twiliovoice/Twiliovoicequickstart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twilio.twiliovoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Twiliovoicequickstart1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
